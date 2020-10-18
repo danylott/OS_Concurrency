9b2912469de31974b5e5dd47047c7a92a5adff46
aoyako <ml.logachevsky@gmail.com>
2020-10-18 19:46:43 +0300

    init

---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    void mainTest() {
+        assertTrue(true);
+    }
+}
